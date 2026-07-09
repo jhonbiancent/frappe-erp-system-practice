f75864bf9096f51ec20ae172bc73c8f29a2f40ad
jhonbiancent <jhonbiancent06@gmail.com>
2026-07-10 00:45:04 +0800

    feat: added membership date and name check

--- library_management/library_management/doctype/library_membership/library_membership.py
+++ library_management/library_management/doctype/library_membership/library_membership.py
@@ -1,20 +1,38 @@
 # Copyright (c) 2026, Jhon Biancent and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
-
+from frappe.model.docstatus import DocStatus
 
 class LibraryMembership(Document):
 	# begin: auto-generated types
 	# This code is auto-generated. Do not modify anything in this block.
 
+	def before_save(self):
+		exists = frappe.db.exists(
+			"Library Membership",
+			{
+				"library_member": self.library_member,
+				"docstatus" : DocStatus.submitted(),
+				# check if the membership's end date is later than this membership's start date
+				"to_date" : (">", self.from_date ),
+			},
+		)
+		if exists: 
+			frappe.throw("There is an active membership for this member")
+	
 	from typing import TYPE_CHECKING
 
 	if TYPE_CHECKING:
 		from frappe.types import DF
 
 		amended_from: DF.Link | None
+		from_date: DF.Date | None
+		library_member: DF.Link
+		name1: DF.Data | None
+		paid: DF.Check
+		to_date: DF.Date | None
 	# end: auto-generated types
 
 	_DOCTYPE_NAME = "Library Membership"
